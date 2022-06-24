e264f8784a6b120d250cce36e7aa64821df12c4c
Jaweinc <awejoseph01@gmail.com>
2022-06-24 21:52:37 +0100

    created gif end point

---
+++ controllers/employee/modules/CreateGif.ts
@@ -0,0 +1,49 @@
+import db from '../../../orm/orm'
+import jwt, { Secret } from 'jsonwebtoken';
+import dotenv from 'dotenv'
+dotenv.config()
+
+const CreateGif = (req: any, res: any) => {
+    console.log(1)
+    jwt.verify(req.token, (process.env.jwtSecret as Secret), async (err: any, authData: any) => {
+        if (err) res.sendStatus(403);
+        const eid = authData.userId
+        const { image, title } = req.body;
+        try {
+            if (err) res.sendStatus(403);
+            const isImage = await db.selectAll('add_gif', {image})
+
+            if (isImage.rows.length === 0) {
+                const addGif = await db.create('add_gif', {
+                    employee_id: eid,
+                    image,
+                    title
+                })
+                console.log(addGif)
+                if (!addGif) {
+                    return res.status(400).json({
+                        status: 'error',
+                        message: 'error Uploading gif',
+                    });
+                }
+            } else {
+                return res.status(400).json({
+                    status: 'error',
+                    message: 'error Uploading gif. gif already exist',
+                });
+            }
+
+            return res.status(200).json({
+                status: 'success',
+                message: 'Gif Uploaded successfully',
+            });
+        } catch {
+            return res.status(400).json({
+                status: 'error',
+                message: 'Ops!!! an error has occured'
+            })
+        }
+    })
+}
+
+export default CreateGif
